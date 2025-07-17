fc5f5a8ce5a470c42e50637bc2c894a1a05cc752
superjeepboy <superjeepboy@hotmail.com>
2025-07-16 21:47:03 -0700

    moved new itemtoggle classes to namespace

--- Content.Shared/_DV/Item/ItemToggle/Systems/ItemToggleExamineSystem.cs
+++ Content.Shared/_DV/Item/ItemToggle/Systems/ItemToggleExamineSystem.cs
@@ -1,7 +1,8 @@
+using Content.Shared._DV.Item.ItemToggle.Components;
 using Content.Shared.Examine;
 using Content.Shared.Item.ItemToggle;
 
-namespace Content.Shared.DeltaV.Item.ItemToggle.Systems;
+namespace Content.Shared._DV.Item.ItemToggle.Systems;
 
 public sealed class ItemToggleExamineSystem : EntitySystem
 {
@@ -11,10 +12,10 @@ public sealed class ItemToggleExamineSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<DeltaV.Item.ItemToggle.Components.ItemToggleExamineComponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<ItemToggleExamineComponent, ExaminedEvent>(OnExamined);
     }
 
-    private void OnExamined(Entity<DeltaV.Item.ItemToggle.Components.ItemToggleExamineComponent> ent, ref ExaminedEvent args)
+    private void OnExamined(Entity<ItemToggleExamineComponent> ent, ref ExaminedEvent args)
     {
         var msg = _toggle.IsActivated(ent.Owner) ? ent.Comp.On : ent.Comp.Off;
         args.PushMarkup(Loc.GetString(msg));
